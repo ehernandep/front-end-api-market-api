6de7323ce46b0b1082b166e5293ce82e6611e066
efhernandez-neostella <ehernandep@eafit.edu.co>
2025-05-25 23:32:12 -0500

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,30 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { AwsRum, AwsRumConfig } from "aws-rum-web";
+
+try {
+  const config: AwsRumConfig = {
+    sessionSampleRate: 1,
+    endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
+    telemetries: ["performance", "errors", "http"],
+    allowCookies: true,
+    enableXRay: false,
+    signing: true, // If you have a public resource policy and wish to send unsigned requests please set this to false
+  };
+
+  const APPLICATION_ID: string = "61f1a7b0-c2ff-4222-a8ce-fd7019b2787d";
+  const APPLICATION_VERSION: string = "1.0.0";
+  const APPLICATION_REGION: string = "us-east-1";
+
+  const awsRum: AwsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
